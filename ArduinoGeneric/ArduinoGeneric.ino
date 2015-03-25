663fd6d616393c521129007c80afd6de36b83bb4
Steven Gann <sgann2012@fau.edu>
2015-03-24 23:28:26 -0400

    Improved Arduino Firmware
    Added linear fading to new value.

--- ArduinoGeneric/ArduinoGeneric.ino
+++ ArduinoGeneric/ArduinoGeneric.ino
@@ -2,19 +2,20 @@
 // Generic Arduino firmware for OpenBacklight //
 //============================================//
 
-int Rpin = 9;
-int Gpin = 10;
-int Bpin = 11;
+int Speed = 100;
+int RedPin = 6;
+int GreenPin = 3;
+int BluePin = 5;
 
-int Rval = 0;
-int Gval = 0;
-int Bval = 0;
+int RedVal = 0;
+int GreenVal = 0;
+int BlueVal = 0;
 
 void setup()
 {
-    pinMode(Rpin, OUTPUT);
-    pinMode(Gpin, OUTPUT);
-    pinMode(Bpin, OUTPUT);
+    pinMode(RedPin, OUTPUT);
+    pinMode(GreenPin, OUTPUT);
+    pinMode(BluePin, OUTPUT);
     
     Serial.begin(115200);
 }
@@ -23,37 +24,63 @@ void loop()
 {
     readPacket();
     
-    analogWrite(Rpin, Rval);
-    analogWrite(Gpin, Gval);
-    analogWrite(Bpin, Bval);
-    
-    Serial.println(String(Rval) + ", " + String(Gval) + ", " + String(Bval)); 
+    //Serial.println(String(Rval) + ", " + String(Gval) + ", " + String(Bval)); 
     
     delay(100);
 }
 
 void readPacket()
 {
-    int Rnew = -1;
-    int Gnew = -1;
-    int Bnew = -1;
-    
     while(Serial.available() > 0) //when there's information being received through serial go into this loop
     {
-      Rnew = Serial.parseInt(); //takes each value seperated by the commas and defines them to the four different values
-      Bnew = Serial.parseInt();
-      Gnew = Serial.parseInt();
+      int Rnew = Serial.parseInt(); //takes each value seperated by the commas and defines them to the four different values
+      int Gnew = Serial.parseInt();
+      int Bnew = Serial.parseInt();
       serialFlush(); //flushes any other values in the buffer
+      
+      FadeTo(Rnew, Gnew, Bnew, 1000);
     }
+
+}
+
+void FadeTo(int redTarget, int greenTarget, int blueTarget, int timeTarget)
+{
+  float redDelta = ((float)(redTarget - RedVal)) / ((float)timeTarget);
+  float greenDelta = ((float)(greenTarget - GreenVal)) / ((float)timeTarget);
+  float blueDelta = ((float)(blueTarget - BlueVal)) / ((float)timeTarget);
+  
+  //Serial.print("Deltas: ");
+  //Serial.print(String(redDelta) + " ");
+  //Serial.print(String(greenDelta) + " ");
+  //Serial.print(String(blueDelta) + " ");
+  //Serial.print("\n");
+  
+  float redNew = (float)RedVal;
+  float greenNew = (float)GreenVal;
+  float blueNew = (float)BlueVal;
+  
+  for (int i = 0; i < timeTarget; i++)
+  {
+    redNew += redDelta;
+    greenNew += greenDelta;
+    blueNew += blueDelta;
     
-    Serial.println(String(Rnew) + ", " + String(Gnew) + ", " + String(Bnew)); 
+    //Serial.print("New values: ");
+    //Serial.print(String(redNew) + " ");
+    //Serial.print(String(greenNew) + " ");
+    //Serial.print(String(blueNew) + " ");
+    //Serial.print("\n");
     
-    if (Rnew >= 0 && Rnew <= 255 && Gnew >= 0 && Gnew <= 255 && Bnew >= 0 && Bnew <= 255)
-        {
-            Rval = Rnew;
-            Gval = Gnew;
-            Bval = Bnew;
-        }
+    analogWrite(RedPin, (int)redNew);
+    analogWrite(GreenPin, (int)greenNew);
+    analogWrite(BluePin, (int)blueNew);
+    
+    delay(1);
+  }
+  
+  RedVal = (int)redNew;
+  GreenVal = (int)greenNew;
+  BlueVal = (int)blueNew;
 }
 
 void serialFlush()
